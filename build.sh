39568efb1e818693c7129b234c0c1a4e2c57522f
MD Raza <raza231198@gmail.com>
2019-07-01 10:52:57 +0530

    build script
    Signed-off-by: MD Raza <raza231198@gmail.com>

---
+++ build.sh
@@ -0,0 +1,22 @@
+# Main Environment
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
+CONFIG=X00PD_defconfig
+CORES=$(grep -c ^processor /proc/cpuinfo)
+THREAD="-j$CORES"
+CROSS_COMPILE+="ccache "
+CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+
+echo -e "\n(i) Cloning toolcahins if folder not exist..."
+git clone https://github.com/raza231198/aarch64-linux-android-4.9
+
+# Export
+export ARCH=arm64
+export SUBARCH=arm64
+export PATH=/usr/lib/ccache:$PATH
+export CROSS_COMPILE
+export KBUILD_BUILD_USER=reza
+export KBUILD_BUILD_HOST=unique
+make  O=out $CONFIG $THREAD
